4e9ed85e764294118527a38d6ee818ee2baf4f34
Masterclaus <claus.schabetsberger@gmail.com>
2013-05-01 21:16:55 +0200

    Added random main test

--- src/at/jku/aig2qbf/test/TestMain.java
+++ src/at/jku/aig2qbf/test/TestMain.java
@@ -1,5 +1,9 @@
 package at.jku.aig2qbf.test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,9 +13,12 @@ import at.jku.aig2qbf.visualizer.TreeVisualizer;
 
 public class TestMain {
 	private final String TEMP_QDIMACS_FILE = "./output/temp.qbf";
+	
+	private Random random;
 
 	@Before
 	public void setUp() throws Exception {
+		this.random = new Random();
 	}
 
 	@After
@@ -22,13 +29,71 @@ public class TestMain {
 	public void test() {
 		TreeVisualizer.CLOSE_ON_EXIT = true;
 		
-		aig2qbf.main(new String[] { "-h" });
+		List<ParameterPair> parameterList = new ArrayList<ParameterPair>();
+		parameterList.add(new ParameterPair("-h"));
+		parameterList.add(new ParameterPair("-k", "5"));
+		parameterList.add(new ParameterPair("-i", "input/basic/and.aag"));
+		parameterList.add(new ParameterPair("-it", "aag"));
+		parameterList.add(new ParameterPair("-nr"));
+		parameterList.add(new ParameterPair("-ns"));
+		parameterList.add(new ParameterPair("-nu"));
+		parameterList.add(new ParameterPair("-o", TEMP_QDIMACS_FILE));
+		parameterList.add(new ParameterPair("-ot", "qbf"));
+		parameterList.add(new ParameterPair("-v"));
+		parameterList.add(new ParameterPair("-vis"));
 		
-		aig2qbf.main(new String[] { "-k", "5", "-i", "input/basic/and.aag", "-it", "aag", "-nr", "-ns", "-nu", "-o", TEMP_QDIMACS_FILE, "-ot", "qbf", "-v" });
+		final int max_iterations = 100;
 		
-		aig2qbf.main(new String[] { "-k", "1", "-i", "input/basic/and.aag", "-nu", "-o", TEMP_QDIMACS_FILE, "-v", "-vis" });
+		for(int i = 0; i < max_iterations; i++) {
+			final int parameterCount = this.random.nextInt(parameterList.size());
+			
+			List<String> selectedParameterList = new ArrayList<String>();
+			
+			for(int j = 0; j < parameterCount; j++) {
+				ParameterPair parameterPair = parameterList.get(this.random.nextInt(parameterList.size()));
+				
+				selectedParameterList.add(parameterPair.key);
+				
+				if(parameterPair.value != null) {
+					selectedParameterList.add(parameterPair.value);
+				}
+			}
+			
+			String[] selectedParameters = new String[selectedParameterList.size()];
+			selectedParameters = selectedParameterList.toArray(selectedParameters);
+			
+			printSelectedParameters(selectedParameters);
+			
+			aig2qbf.main(selectedParameters);
+		}
+	}
+	
+	private void printSelectedParameters(String[] parameters) {
+		System.out.print("Selected parameters: ");
 		
-		aig2qbf.main(new String[] { "-k", "9", "-i", "input/basic/and.aag", "-o", TEMP_QDIMACS_FILE, "-vis" });
+		for(int i = 0; i < parameters.length; i++) {
+			System.out.print(parameters[i]);
+			
+			if(i < parameters.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		
+		System.out.println();
 	}
 
+	private class ParameterPair {
+		String key;
+		String value;
+		
+		public ParameterPair(String key) {
+			this.key = key;
+			this.value = null;
+		}
+		
+		public ParameterPair(String key, String value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
 }
